ac5fe12574f8ed20d6a24f9d27cb828a51766e15
jefferson2005 <104094429+jefferson2005@users.noreply.github.com>
2023-01-30 15:09:16 -0300

    Create teste.php

---
+++ teste.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+	<html lang="pt-br">
+		<head>
+			<title>Teste da Linguagem PHP</title>
+			<meta charset="utf-8">
+		</head>
+		<body>
+			<?php 
+				echo '<h1>Teste da Linguagem HTML (HyperText Markup Language)</h1>';
+				echo 'Autor: Jefferson L<br>';
+				echo 'Site: procedimentosemti.com.br<br>';
+				echo 'Instagram:jeffersonlins98<br>';
+				echo 'marketing: youtube.com/BoraParaPratica<br>'; 
+                <img src="./TempleOs.png" alt="Religião do God vava">
+			?>
+		</body>
+	</html>
+    
